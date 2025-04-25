48f9769ad48249a89912eb2d657586b996a62e6f
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2025-04-25 13:53:51 +0700

    feat: avoid race conditions with transactions

--- src/controllers/user_movie_controller.js
+++ src/controllers/user_movie_controller.js
@@ -1,25 +1,45 @@
 const response = require("../helpers/response");
-
-const { addMovieToUserModel } = require("../models/user_movie_model");
-const {
-  getMovieModel,
-  updateStockMovieModel,
-} = require("../models/movie_model");
+const db = require("../helpers/db");
 
 const addMovieToUser = async (req, res) => {
   const currentUser = req.user.username;
   const bookdId = req.body.bookId;
 
+  const connection = await db.getConnection();
+
   try {
-    const book = await getMovieModel(bookdId);
+    // begin the transaction
+    await connection.beginTransaction();
+
+    // get book based on id with FOR UPDATE
+    const [book] = await connection.query(
+      "SELECT * FROM movies WHERE id = ? FOR UPDATE",
+      [bookdId]
+    );
+
+    // Check if book is exist
     if (!book.length) {
       return response(res, "Book not found", 400, false, null);
     }
+
+    // Check book stock
     if (book[0]?.stock <= 0) {
       return response(res, "Book out of stock", 400, false, null);
     }
-    const result = await addMovieToUserModel(currentUser, bookdId);
-    await updateStockMovieModel(bookdId);
+
+    // simulate race conditions
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    /* add book to user */
+    const result = await connection.query(
+      "INSERT INTO user_movie (username, movie_id) VALUES (?, ?)",
+      [currentUser, bookdId]
+    );
+    await connection.query("UPDATE movies SET stock = stock - 1 WHERE id = ?", [
+      bookdId,
+    ]);
+
+    await connection.commit();
     return response(res, "Success", 200, true, result);
   } catch (err) {
     return response(res, "Error", 500, false, err);
